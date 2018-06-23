35f3c849f3bb744dce49db32999a74e66b1c142c
Komlósi Péter <komlosi.peti@stud.uni-obuda.hu>
2018-06-23 23:13:03 +0200

    improved animations

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 (function () {
     window.inView.threshold(0.3);
 
-    // Reveal main divs
-    window.inView('.main__content-item')
-        .on('enter', el => {
-            el.classList.remove("is-hidden")
-        })
+    setTimeout(() => {
+        // Reveal the first paragraph
+        document.getElementsByClassName("main_content-item")[0].classList.remove("is-hidden");
+    }, 750);
 
-    // Reveal sidebar divs
-    window.inView('.sidebar__section')
-        .on('enter', el => {
-            el.classList.remove("is-hidden")
-        })
+    setTimeout(() => {
+        // Reveal main divs
+        window.inView(".main_content-item")
+            .on("enter", el => {
+                el.classList.remove("is-hidden")
+            })
+    }, 500);
+
+    setTimeout(() => {
+        // Slide in sidebar
+        // document.getElementsByClassName("sidebar")[0].classList.add("reveal");
+        // Reveal section titles
+        window.inView(".section_title")
+            .on("enter", el => {
+                el.classList.remove("is-hidden")
+            })
+        // Reveal sidebar divs
+        window.inView(".sidebar_section")
+            .on("enter", el => {
+                el.classList.remove("is-hidden")
+            })
+    }, 250);
 })();
