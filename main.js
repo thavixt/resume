f6aec42497b147a926114ef2382bde7eadcf0719
Peter Komlosi <komlosi.peti@gmail.com>
2020-06-03 01:31:18 +0200

    minor updates

--- main.js
+++ main.js
@@ -1,45 +1,38 @@
 (function () {
-    // window.scrollTo(0, 0);
+  window.onbeforeunload = () => window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 
-    // https://github.com/camwiegert/in-view
-    window.inView.threshold(0.5);
-    const delay = 1000;
+  // https://github.com/camwiegert/in-view
+  window.inView.threshold(0.5);
+  const delay = 1000;
 
-    setTimeout(() => {
-        // Reveal the first paragraph
-        document.getElementsByClassName("main_content-item")[0].classList.remove("is-hidden");
-    }, delay + 750);
+  setTimeout(() => {
+    // Reveal the first paragraph
+    document.getElementsByClassName("main_content-item")[0].classList.remove(
+      "is-hidden",
+    );
+  }, delay + 750);
 
-    setTimeout(() => {
-        // Reveal main divs
-        window.inView(".main_content-item")
-            .on("enter", el => {
-                el.classList.remove("is-hidden")
-            })
-    }, delay + 500);
+  setTimeout(() => {
+    // Reveal main divs
+    window.inView(".main_content-item")
+      .on("enter", (el) => {
+        el.classList.remove("is-hidden");
+      });
+  }, delay + 500);
 
-    setTimeout(() => {
-        // Slide in sidebar
-        document.getElementsByClassName("sidebar")[0].classList.remove("is-hidden");
-        // Reveal section titles
-        window.inView(".section_title")
-            .on("enter", el => {
-                el.classList.remove("is-hidden")
-            })
-        // Reveal sidebar divs
-        window.inView(".sidebar_section")
-            .on("enter", el => {
-                el.classList.remove("is-hidden")
-            })
-    }, delay + 250);
-
-    // https://github.com/makotot/scrollspy
-    /* const mainSpy = new ScrollSpy('#main-spy', {
-        nav: '#main-spy > ul > li > a',
-        className: 'active'
-    });
-    const sideSpy = new ScrollSpy('#side-spy', {
-        nav: '#side-spy > ul > li > a',
-        className: 'active'
-    }); */
+  setTimeout(() => {
+    // Slide in sidebar
+    document.getElementsByClassName("sidebar")[0].classList.remove("is-hidden");
+    // Reveal section titles
+    window.inView(".section_title")
+      .on("enter", (el) => {
+        el.classList.remove("is-hidden");
+      });
+    // Reveal sidebar divs
+    window.inView(".sidebar_section")
+      .on("enter", (el) => {
+        el.classList.remove("is-hidden");
+      });
+  }, delay + 250);
 })();
